bcc32355f1f5cb78ea040e8f42d4344ab3f6a88c
goastcjh <545660123@qq.com>
2019-11-26 21:18:25 +0800

    quicksort

--- sort.cc
+++ sort.cc
@@ -6,12 +6,15 @@ void print(int a[], int len);
 void BubbleSort(int a[], int len);
 void SelectionSort(int a[], int len);
 void ShellSort(int a[], int len);
-void QuickSort(int a[], int len);
+void QuickSort(int a[], int l, int r);
 
 int main() {
     int a[6] = {2,5,4,3,1,7};
     BubbleSort(a,6);
     SelectionSort(a,6);
+    ShellSort(a,6);
+    QuickSort(a,0,6);
+    print(a,6);
     return 0;
 }
 
@@ -57,20 +60,73 @@ void SelectionSort(int a[], int len) {
 }
 
 void InsertSort(int a[], int len) {
-    for(int i = 0; i < len; i++) {
-        for(int j = 0; j < len; j++) {
-            
+    int temp = 0;
+    for(int i = 0; i < len-1; i++) {
+        for(int j = i+1; j > 0; j--) {
+            if(a[j] < a[j-1]) {
+                temp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = temp;
+            }
+            else
+                break;
         }
     }
     return;
 }
 
 void ShellSort(int a[], int len) {
+    int temp = 0;
+    int incre = len;
 
+    while(1) {
+        incre = incre / 2;
+        for(int k = 0; k < incre; k++) {
+            for(int i = k+incre; i < len; i += incre) {
+                for(int j = i; j > k; j -= incre) {
+                    if(a[j] < a[j-incre]) {
+                        temp = a[j-incre];
+                        a[j-incre] = a[j];
+                        a[j] = temp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(incre == 1)
+            break;
+    }
+    print(a, len);
     return;
 }
 
-void QuickSort(int a[], int len) {
+void QuickSort(int a[], int l, int r) {
+    if(r <= 1) {
+        return;
+    }
+    int i = l, j = r, key = a[l];
     
+    while(i < j) {
+        while(i < j && a[j] <= key)
+            j--;
+        if(i < j) {
+            a[i] = a[j];
+            i++;
+        }
+        
+        while(i < j && a[i] >= key)
+            i++;
+        if(i < j) {
+            a[j] = a[i];
+            j--;
+        }
+
+        //i == j
+        a[i] = key;
+        QuickSort(a,l,i-1);
+        QuickSort(a,i+1,r);
+    }
     return;
 }
